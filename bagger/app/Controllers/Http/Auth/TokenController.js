d22904bd9c359542fcba03e7e13612eeddd16e5f
Muhammad Syahmi Bin Ahmad Kamal <syahmi.ak@okwave.global>
2021-12-08 15:17:01 +0800

    [FEAT] TokenController: VerifyResetToken() function

--- bagger/app/Controllers/Http/Auth/TokenController.js
+++ bagger/app/Controllers/Http/Auth/TokenController.js
@@ -48,8 +48,53 @@ class TokenController {
             return response.status(500).send({
                 'status': false,
                 'message': 'Internal server error',
-                'data': 'VerifyToken error. ' + error.toString() + '. (TokenController.VerifyToken)'
+                'data' : '',
+                'error': 'VerifyToken error. ' + error.toString() + '. (TokenController.VerifyToken)'
             })
+        }
+    }
+
+    async VerifyResetToken({ request, response }) {
+        try {
+
+            const validation = await validateAll(request.all(), {
+                resetToken: 'required'
+            });
+
+            if(validation.fails()) { 
+                return response.status(400).send({
+                    'message': validation.messages(),
+                    'detail': '', 
+                });
+            } else {
+
+                //retrieve input
+                const { resetToken } = await request.all();
+                const resetTokenExisted = await User.query().where('resetToken', resetToken).first();
+
+                //if token not existed
+                if(!resetTokenExisted) {
+                    return response.status(200).send({
+                        'status' : false,
+                        'message' : 'Invalid token. Click URL in your email.',
+                        'data' : ''
+                    });
+                } else {
+                    return response.status(200).send({
+                        'status' : true,
+                        'message' : 'Success to verify token.',
+                        'data' : ''
+                    });
+                }
+            }
+
+        } catch(error) { 
+            return response.status(500).send({
+                'status' : false,
+                'message' : 'Internal server error',
+                'data' : '',
+                'error' : 'Verify reset token error. ' + error.toString() + '. (TokenController.VerifyResetToken)'
+            });
         }
     }
 }
