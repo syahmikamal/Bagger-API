3127de98e85283652f80729a2f48208c83570f5d
Muhammad Syahmi Bin Ahmad Kamal <syahmi.ak@okwave.global>
2021-12-16 21:35:09 +0800

    [FIX] UserController: Change a bit logic for sent email - reset password

--- bagger/app/Controllers/Http/Auth/UserController.js
+++ bagger/app/Controllers/Http/Auth/UserController.js
@@ -22,7 +22,7 @@ class UserController {
             const Validation = await validateAll(request.all(), {
                 personName: 'required|min:3|max:32',
                 username: 'required|min:3|max:15',
-                email: 'required|email|max:100',          
+                email: 'required|email|max:100',
                 password: [
                     rule('required'),
                     rule('min', 8),
@@ -54,11 +54,11 @@ class UserController {
 
                     var objectSend = {
                         'url': 'http://127.0.0.1:4200/verify-token/' + verifyToken,
-                        'username' : username
+                        'username': username
                     };
 
                     //TODO: Send mail as notification for signing up
-                    await Mail.send('emails.welcome',objectSend , (message) => {
+                    await Mail.send('emails.welcome', objectSend, (message) => {
                         message
                             .to(email)
                             .from(Env.get('DEFAULT_FROM_EMAIL'))
@@ -108,7 +108,7 @@ class UserController {
             return response.status(500).send({
                 'status': false,
                 'message': 'Internal server error',
-                'data' : '',
+                'data': '',
                 'error': 'SignUp error. ' + error.toString() + '. (UserController.SignUp)'
             })
         }
@@ -165,34 +165,34 @@ class UserController {
                         'status': true,
                         'message': 'Successfully sign in',
                         'data': {
-                            'email'    : email,
-                            'userId'   : userId,
-                            'name'     : userData.name,
-                            'jwtToken' : generateJWT
+                            'email': email,
+                            'userId': userId,
+                            'name': userData.name,
+                            'jwtToken': generateJWT
                         }
                     })
                 } else {
                     return response.status(200).json({
-                        'status' : false,
-                        'message' : 'Invalid user credentials. Password does not match with current password',
-                        'data'  : ''
+                        'status': false,
+                        'message': 'Invalid user credentials. Password does not match with current password',
+                        'data': ''
                     });
                 }
 
             } else {
                 return response.status(200).json({
-                    'status' : false,
+                    'status': false,
                     'message': 'Email does not exist',
-                    'data'   : ''
+                    'data': ''
                 });
             }
 
         } catch (error) {
 
             return response.status(500).send({
-                'status' : false,
+                'status': false,
                 'message': 'Internal server error',
-                'data'   : 'Sign in error. ' + error.toString() + '. (UserController.SignIn)'
+                'data': 'Sign in error. ' + error.toString() + '. (UserController.SignIn)'
             });
         }
     }
@@ -201,58 +201,57 @@ class UserController {
         try {
 
             const validation = await validateAll(request.all(), {
-                resetEmail : 'required|email'
+                resetEmail: 'required|email'
             });
 
-            if(validation.fails()) {
+            if (validation.fails()) {
                 return response.status(400).send({
-                    'status' : false,
-                    'message' : validation.messages(),
-                    'data' : ''
+                    'status': false,
+                    'message': validation.messages(),
+                    'data': ''
                 });
 
             } else {
 
                 //retrieve input
-                const {resetEmail} = await request.all();
+                const { resetEmail } = await request.all();
                 const userData = await User.query().where('email', resetEmail).first();
 
-                console.log('user: ', userData.isActive);
-
-                if(userData.isActive == false) { 
-                    return response.status(400).send({
-                        'status' : false,
-                        'message' : 'Unverified user',
-                        'data' : ''
-                    });
-                }
 
                 //check the existing email
-                if(userData) {
+                if (userData) {
+
+                    if (userData.isActive == false) {
+                        return response.status(400).send({
+                            'status': false,
+                            'message': 'Unverified user',
+                            'data': ''
+                        });
+                    }
 
                     const resetToken = await Generate.random('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789', 64);
 
-                    await Database.from('users').where({'email': resetEmail}).update({'resetToken':resetToken});
+                    await Database.from('users').where({ 'email': resetEmail }).update({ 'resetToken': resetToken });
 
                     const userInfo = await Person.query().where('person_id', userData.user_id).first();
 
                     //TODO Send email through adonisjs email 
 
                     return response.status(200).send({
-                        'status' : true,
-                        'message' : 'Successfully reset password - email sent!',
-                        'data' : {
-                            'name' : userInfo.name,
-                            'resetPassword' : resetToken
+                        'status': true,
+                        'message': 'Successfully reset password - email sent!',
+                        'data': {
+                            'name': userInfo.name,
+                            'resetPassword': resetToken
                         }
                     });
 
                 } else {
 
                     return response.status(200).send({
-                        'status' : false,
-                        'message' : 'User is not existed',
-                        'data' : ''
+                        'status': false,
+                        'message': 'Email does not exist',
+                        'data': ''
                     });
                 }
 
@@ -261,104 +260,104 @@ class UserController {
 
         } catch (error) {
             return response.status(500).send({
-                'status' : false,
-                'message' : 'Internal server error',
-                'data' : 'Send email reset password error. ' + error.toString() + '. (UserController.SendResetMail)'
+                'status': false,
+                'message': 'Internal server error',
+                'data': 'Send email reset password error. ' + error.toString() + '. (UserController.SendResetMail)'
             });
         }
     }
 
-   async ResetPassword ({ request, response }) {
-       try {
+    async ResetPassword({ request, response }) {
+        try {
 
-        const messages = {
-            'resetToken.required' : 'Reset token is required',
+            const messages = {
+                'resetToken.required': 'Reset token is required',
 
-            'newPassword.required'  : 'Password is required',
-            'newPassword.min'       : 'Password length must be more than 8 characters',
-            'newPassword.max'       : 'Password length must be less than 18 characters',
-            'newPassword.regex'     : 'Password must includes at least 1 lowercase letter, 1 uppercase letter, 1 number and 1 special characters (!@#$%^&_) and no whitespace',
+                'newPassword.required': 'Password is required',
+                'newPassword.min': 'Password length must be more than 8 characters',
+                'newPassword.max': 'Password length must be less than 18 characters',
+                'newPassword.regex': 'Password must includes at least 1 lowercase letter, 1 uppercase letter, 1 number and 1 special characters (!@#$%^&_) and no whitespace',
 
-            'retypePassword.required'       : 'Retype password is required',
-            'retypePassword.min'            : 'Retype password length must be more than 8 characters',
-            'retypePassword.max'            : 'Retype password length must be less than 18 characters',
-            'retypePassword.regex'          : 'Retype password must includes at least 1 lowercase letter, 1 uppercase letter, 1 number and 1 special characters (!@#$%^&_) and no whitespace.'
-        }
+                'retypePassword.required': 'Retype password is required',
+                'retypePassword.min': 'Retype password length must be more than 8 characters',
+                'retypePassword.max': 'Retype password length must be less than 18 characters',
+                'retypePassword.regex': 'Retype password must includes at least 1 lowercase letter, 1 uppercase letter, 1 number and 1 special characters (!@#$%^&_) and no whitespace.'
+            }
 
-        const validation = await validateAll(request.all(), {
-            resetToken: 'required',
-            newPassword: [
-                rule('required'),
-                rule('min', 8),
-                rule('max', 18),
-                rule('regex', /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&_])(^\S*$)/)
-            ],
-            retypePassword: [
-                rule('required'),
-                rule('min', 8),
-                rule('max', 18),
-                rule('regex', /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&_])(^\S*$)/)
-            ]
-        }, messages);
+            const validation = await validateAll(request.all(), {
+                resetToken: 'required',
+                newPassword: [
+                    rule('required'),
+                    rule('min', 8),
+                    rule('max', 18),
+                    rule('regex', /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&_])(^\S*$)/)
+                ],
+                retypePassword: [
+                    rule('required'),
+                    rule('min', 8),
+                    rule('max', 18),
+                    rule('regex', /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&_])(^\S*$)/)
+                ]
+            }, messages);
 
-        if(validation.fails()) { 
-            return response.status(400).send({
-                'status': false,
-                'message': validation.messages(),
-                'detail': ''
-            });
-        } else {
+            if (validation.fails()) {
+                return response.status(400).send({
+                    'status': false,
+                    'message': validation.messages(),
+                    'detail': ''
+                });
+            } else {
 
-            const { resetToken, newPassword, retypePassword } = await request.all();
-            const userData = await User.query().where('resetToken', resetToken).first();
+                const { resetToken, newPassword, retypePassword } = await request.all();
+                const userData = await User.query().where('resetToken', resetToken).first();
 
-            //Check the existance of reset token
-            if(userData) {
+                //Check the existance of reset token
+                if (userData) {
 
-                //Check the match between retype passward with new password
-                if(newPassword === retypePassword) {
+                    //Check the match between retype passward with new password
+                    if (newPassword === retypePassword) {
 
-                    const newHashPassword = await Hash.make(newPassword);
+                        const newHashPassword = await Hash.make(newPassword);
 
-                    //update user table
-                    await Database.from('users').where({'resetToken': resetToken}).update({'password': newHashPassword});
-                    await Database.from('users').where({'resetToken': resetToken}).update({'resetToken': null});
+                        //update user table
+                        await Database.from('users').where({ 'resetToken': resetToken }).update({ 'password': newHashPassword });
+                        await Database.from('users').where({ 'resetToken': resetToken }).update({ 'resetToken': null });
 
-                    return response.status(200).send({
-                        'status' : true,
-                        'message': 'Success reset password',
-                        'data' : '',
-                        'error' : ''
-                    });
+                        return response.status(200).send({
+                            'status': true,
+                            'message': 'Success reset password',
+                            'data': '',
+                            'error': ''
+                        });
+
+                    } else {
+                        return response.status(200).send({
+                            'status': false,
+                            'message': 'Retype password is not matched',
+                            'data': '',
+                            'error': ''
+                        });
+                    }
 
                 } else {
                     return response.status(200).send({
-                        'status'    : false,
-                        'message'   : 'Retype password is not matched',
-                        'data'      : '',
-                        'error'     :''
+                        'status': false,
+                        'message': 'Invalid token credentials.',
+                        'data': '',
+                        'error': ''
                     });
                 }
-
-            } else {
-                return response.status(200).send({
-                    'status' : false,
-                    'message' : 'Invalid token credentials.',
-                    'data' : '',
-                    'error': ''
-                });
             }
-        }
 
-       } catch (error) {
-           return response.status(500).send({
-               'status' : false,
-               'message' : 'Internal server error',
-               'data' : '',
-               'error' : 'Reset password error. ' + error.toString() + '. (UserController.ResetPassword)'
-           });
-       }
-   }
+        } catch (error) {
+            return response.status(500).send({
+                'status': false,
+                'message': 'Internal server error',
+                'data': '',
+                'error': 'Reset password error. ' + error.toString() + '. (UserController.ResetPassword)'
+            });
+        }
+    }
 
 }
 
