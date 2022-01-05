4b4953757088e01912ab8e9b9acdf61c8b90b194
Muhammad Syahmi Bin Ahmad Kamal <syahmi.ak@okwave.global>
2022-01-05 23:44:39 +0800

    [FEAT] Migration: Post model and migration table

---
+++ bagger/database/migrations/1641365723330_post_schema.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class PostSchema extends Schema {
+  up () {
+    this.create('posts', (table) => {
+      table.increments()
+      table.string('post_id').notNullable().unique()
+      table.string('bagger_id').unique()
+      table.string('post_title').nullable()
+      table.longText('post_content').nullable()
+      table.boolean('post_status').defaultTo(false)
+      table.string('post_subcategory_id')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('posts')
+  }
+}
+
+module.exports = PostSchema
