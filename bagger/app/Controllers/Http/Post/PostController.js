0c063f8bc6837dd51f3650e18cd8ae56579b2015
Muhammad Syahmi Bin Ahmad Kamal <syahmi.ak@okwave.global>
2022-01-06 16:10:57 +0800

    [WIP] PostController: PostContent()

--- bagger/app/Controllers/Http/Post/PostController.js
+++ bagger/app/Controllers/Http/Post/PostController.js
@@ -1,7 +1,12 @@
 'use strict'
 
 const Databse = use('Database');
+const Generate = use('App/Modules/RandomNumber')
 const { validateAll, rule } = use('Validator')
+
+const User = use('App/Models/User')
+const Post = use('App/Models/Post')
+const Person = use('App/Models/Person')
 
 class PostController {
 
@@ -9,10 +14,10 @@ class PostController {
         try {
 
             const Validation = await validateAll(request.all(), {
-                
+                email: 'required|email',
                 postContent: 'required|min:5|string',
                 postTitle: 'required|min:3|string:',
-                subcategoryId: 'required'
+                categoryId: 'required'
             })
 
             if (Validation.fails()) {
@@ -22,7 +27,44 @@ class PostController {
                 })
             }
 
-            const { postContent, postTitle, subcategoryId } = await request.all()
+            const { postContent, postTitle, categoryId, email } = await request.all()
+
+            const postID = await Generate.random('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789', 64);
+
+            //check user validity
+            var userIsValid = await User.query().where('email', email).first()
+
+            if (userIsValid) {
+
+                //save in database - post
+                const PostDB = await Post.create({
+                    'bagger_id': userIsValid.user_id,
+                    'post_id': postID,
+                    'post_title': postTitle,
+                    'post_content': postContent,
+                    'post_status': true,
+                    'post_category_id': categoryId
+                })
+
+                //query user info
+                var userInfo = await Person.query().where('person_id', userIsValid.user_id).first();
+
+                return response.status(200).send({
+                    'status': true,
+                    'message': 'Successfully posted a post',
+                    'data': {
+                        'post': PostDB,
+                        'userData': userInfo
+                    }
+                })
+
+            } else {
+                return response.status(200).send({
+                    'status': false,
+                    'message': 'Unexisted email'
+                })
+            }
+
 
         } catch (error) {
             return response.status(500).send({
