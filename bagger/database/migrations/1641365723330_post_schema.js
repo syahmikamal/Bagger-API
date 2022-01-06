f06aa7eb8826b1d1d5321b10d12e9a3b5cffd7fc
Muhammad Syahmi Bin Ahmad Kamal <syahmi.ak@okwave.global>
2022-01-06 16:12:17 +0800

    [FIX] migration: post schema

--- bagger/database/migrations/1641365723330_post_schema.js
+++ bagger/database/migrations/1641365723330_post_schema.js
@@ -7,12 +7,12 @@ class PostSchema extends Schema {
   up () {
     this.create('posts', (table) => {
       table.increments()
-      table.string('post_id').notNullable().unique()
       table.string('bagger_id').unique()
+      table.string('post_id').notNullable().unique()
       table.string('post_title').nullable()
       table.longText('post_content').nullable()
       table.boolean('post_status').defaultTo(false)
-      table.string('post_subcategory_id')
+      table.string('post_category_id')
       table.timestamps()
     })
   }
