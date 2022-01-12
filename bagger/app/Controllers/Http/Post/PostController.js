4f6fd38c1b72e5c0870068fb75f446a0096a5ba9
Muhammad Syahmi Bin Ahmad Kamal <syahmi.ak@okwave.global>
2022-01-12 18:32:29 +0800

    [FEAT] PostController: Image header

--- bagger/app/Controllers/Http/Post/PostController.js
+++ bagger/app/Controllers/Http/Post/PostController.js
@@ -7,6 +7,7 @@ const { validateAll, rule } = use('Validator')
 const User = use('App/Models/User')
 const Post = use('App/Models/Post')
 const Person = use('App/Models/Person')
+const HeaderImage = use('App/Models/HeaderImage')
 
 class PostController {
 
@@ -17,7 +18,8 @@ class PostController {
                 email: 'required|email',
                 postContent: 'required|min:5|string',
                 postTitle: 'required|min:3|string:',
-                categoryId: 'required'
+                categoryId: 'required',
+                headerImage: 'required|string'
             })
 
             if (Validation.fails()) {
@@ -27,7 +29,7 @@ class PostController {
                 })
             }
 
-            const { postContent, postTitle, categoryId, email } = await request.all()
+            const { postContent, postTitle, categoryId, email, headerImage } = await request.all()
 
             const postID = await Generate.random('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789', 64);
 
@@ -44,7 +46,13 @@ class PostController {
                     'post_content': postContent,
                     'post_status': true,
                     'post_category_id': categoryId
-                })
+                });
+
+                //headerImage database
+                const ImageDB = await HeaderImage.create({
+                    'post_id': postID,
+                    'header_image': headerImage
+                });
 
                 //query user info
                 var userInfo = await Person.query().where('person_id', userIsValid.user_id).first();
@@ -71,7 +79,52 @@ class PostController {
                 'status': false,
                 'message': 'Internal server error',
                 'data': '',
-                'error': 'PostContent error. '+error.toString() + '. (PostController.PostContent)'
+                'error': 'PostContent error. ' + error.toString() + '. (PostController.PostContent)'
+            })
+        }
+    }
+
+    async ViewListPost({ request, response }) {
+        try {
+
+            const listPost = await Database.from('posts')
+                .innerJoin('users', function () {
+                    this.on('posts.bagger_id', 'users.user_id')
+                })
+                .innerJoin('people', function () {
+                    this.on('posts.bagger_id', 'people.person_id')
+                })
+                .innerJoin('categories', function () {
+                    this.on('posts.post_category_id', 'categories.category_id')
+                })
+                .innerJoin('header_images', function() {
+                    this.on('posts.post_id', 'header_images.post_id')
+                })
+                .select('posts.id', 'posts.post_id', 'post_title', 'post_content', 'posts.created_at', 'name', 'username', 'category_name', 'header_image')
+                .orderBy('created_at', 'desc')
+
+                if(!listPost) {
+                    return response.status(200).send({
+                        'status': false,
+                        'message': 'Null view list posted content.',
+                        'data': ''
+                    })
+                }
+
+            return response.status(200).send({
+                'status': true,
+                'message': 'Successfully view list posted contents.',
+                'data': {
+                    'listPost': listPost
+                }
+            })
+
+
+        } catch (error) {
+            return response.status(500).send({
+                'status': false,
+                'message': 'Internal server error',
+                'data': 'View list post error. ' + error.toString() + '. (PostController.ViewListPost)'
             })
         }
     }
@@ -83,7 +136,7 @@ class PostController {
                 postId: 'required'
             });
 
-            if(validation.fails()) {
+            if (validation.fails()) {
                 return response.status(400).send({
                     'status': false,
                     'message': validation.messages(),
@@ -95,16 +148,19 @@ class PostController {
 
             //Query post info
             const postData = await Database.from('posts')
-                                .innerJoin('users', function() {
-                                    this.on('posts.bagger_id', 'users.user_id')
-                                })
-                                .innerJoin('people', function() {
-                                    this.on('posts.bagger_id', 'people.person_id')
-                                })
-                                .where('posts.post_id', postId)
-                                .select('posts.id', 'post_id', 'post_title', 'post_content', 'posts.created_at', 'name', 'username')
-            
-            
+                .innerJoin('users', function () {
+                    this.on('posts.bagger_id', 'users.user_id')
+                })
+                .innerJoin('people', function () {
+                    this.on('posts.bagger_id', 'people.person_id')
+                })
+                .innerJoin('categories', function () {
+                    this.on('posts.post_category_id', 'categories.category_id')
+                })
+                .where('posts.post_id', postId)
+                .select('posts.id', 'post_id', 'post_title', 'post_content', 'posts.created_at', 'name', 'username', 'category_name')
+
+
             return response.status(200).send({
                 'status': true,
                 'message': 'Successfully view a post',
